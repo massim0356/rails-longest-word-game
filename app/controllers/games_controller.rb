ba914a189d29fc527ff94a6a219196867db4b10d
Massim Shah <massimshah@hotmail.com>
2021-05-18 11:15:06 +0900

    did some stuff

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 
 class GamesController < ApplicationController
   def new
-    @letters = ("a".."z").to_a.sample(10)
+    @letters = ('a'..'z').to_a.sample(10)
   end
 
   def score
